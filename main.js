465362d33d966ffc03b3c3773e69b7d2b88c978b
raghavravishankar <92210619+raghavravishankar@users.noreply.github.com>
2022-05-11 17:35:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+status = "";
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    }
+
+    function draw() {
+        image(video, 0, 0, 380, 380);
+    }
+
+    function start() {
+        ObjectDetector = ml5.objectDetector('cocossd', modelLoaded);
+        document.getElementById("status").innerHTML = "Status : Detecting Objects";
+        object_name = document.getElementById("object_name").value;
+    }
+
+    function modelLoaded() {
+            console.log("Model Loaded!");
+            status = true;
+    }
+
+function gotResult(error, results) {
+    if (error) {
+        
+    }
+}
